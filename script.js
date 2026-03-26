b7c5613f70e9f436c1b8231c87ae50280aae53aa
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-03-26 17:34:25 +0100

    creata lista email invitati

---
+++ script.js
@@ -0,0 +1,14 @@
+// chiedo all'utente quale esercizio vuole eseguire
+const scelta = prompt("Vuoi eseguire MAIL o DADI?").toLowerCase();
+
+// ESERCIZIO 1 - MAIL
+
+if (scelta === "mail") {
+
+  // creata lista email invitati
+  const listaEmail = [
+    "luigi@gmail.com",
+    "anna@gmail.com",
+    "alfonso@gmail.com",
+    "giada@gmail.com"
+  ];
