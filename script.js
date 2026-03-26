80839e24edb65e4b4c1d38a9d336843fb5abbdc2
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-03-26 18:11:40 +0100

    implementato controllo accesso email con ciclo for, primo esercizio terminato

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // chiedo all'utente quale esercizio vuole eseguire
-const scelta = prompt("Vuoi eseguire MAIL o DADI?").toLowerCase();
+const scelta = prompt("Vuoi eseguire mail o dadi?").toLowerCase();
 
 // ESERCIZIO 1 - MAIL
 
@@ -14,3 +14,25 @@ if (scelta === "mail") {
   ];
 // chiedo all'utente di inserire la propria email
   const emailUtente = prompt("Inserisci la tua email");
+
+ // variabile per verificare se l'email è presente
+  let trovato = false;
+
+  // ciclo for per scorrere tutta la lista email
+  for (let i = 0; i < listaEmail.length; i++) {
+
+    // confronto email utente con ogni email della lista
+    if (emailUtente === listaEmail[i]) {
+      trovato = true; // email trovata
+    }
+
+  }
+
+  // controllo finale accesso
+  if (trovato === true) {
+    console.log("Accesso consentito");
+  } else {
+    console.log("Accesso negato");
+  }
+
+}
